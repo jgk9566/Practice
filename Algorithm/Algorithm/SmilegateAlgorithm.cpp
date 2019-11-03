53fd3ea1127f86549fbdfc15fbb4f5b89604f9b8
김준곤 <jgk9566@gmail.com>
2019-11-04 02:49:25 +0900

    aa

--- Algorithm/Algorithm/SmilegateAlgorithm.cpp
+++ Algorithm/Algorithm/SmilegateAlgorithm.cpp
@@ -162,34 +162,6 @@ void CSmilegateAlgorithm::Triangle()
 	}
 }
 
-typedef struct _tagUIButton
-{
-	unsigned int L, R, B, T;
-	unsigned int iButtonNumber;
-	unsigned int iClickedCount;
-
-	_tagUIButton()	:
-		L(0),
-		R(0),
-		B(0),
-		T(0),
-		iButtonNumber(0),
-		iClickedCount(0)
-	{
-	}
-}UIButton, *PUIButton;
-
-typedef struct _tagClickPos
-{
-	unsigned int X, Y;
-
-	_tagClickPos()	:
-		X(0),
-		Y(0)
-	{
-	}
-}ClickPos, *PClickPos;
-
 void CSmilegateAlgorithm::UIEvent()
 {
 	char cInputNM[100] = {};
--- Algorithm/Algorithm/SmilegateAlgorithm.h
+++ Algorithm/Algorithm/SmilegateAlgorithm.h
@@ -1,14 +1,33 @@
 #pragma once
 #include <vector>
 
-typedef struct _tagRectangle
+typedef struct _tagUIButton
 {
-	unsigned int L;
-	unsigned int R;
-	unsigned int B;
-	unsigned int T;
-}Rectangle, *PRectangle;
+	unsigned int L, R, B, T;
+	unsigned int iButtonNumber;
+	unsigned int iClickedCount;
 
+	_tagUIButton() :
+		L(0),
+		R(0),
+		B(0),
+		T(0),
+		iButtonNumber(0),
+		iClickedCount(0)
+	{
+	}
+}UIButton, *PUIButton;
+
+typedef struct _tagClickPos
+{
+	unsigned int X, Y;
+
+	_tagClickPos() :
+		X(0),
+		Y(0)
+	{
+	}
+}ClickPos, *PClickPos;
 
 class CSmilegateAlgorithm
 {
@@ -21,12 +40,5 @@ public:
 	void CellExpansion();
 	void Triangle();
 	void UIEvent();
-
-private:
-	char* StrTok(char * string, const char * change, char **context);
-
-	
-
-
 };
 
